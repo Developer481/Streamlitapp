50d6723c65c4e8bae731afeadb90bc4f2c27c79a
Developer481 <124763573+Developer481@users.noreply.github.com>
2023-06-09 16:27:08 +0530

    5update

--- stapp.py
+++ stapp.py
@@ -1,12 +1,62 @@
 import streamlit as st
-from pdfgen import Topdftool
+import os
+import multiprocessing
+from bs4 import BeautifulSoup
+import urllib.request
+from xhtml2pdf import pisa
+import shutil
 from django.http import JsonResponse
 
 
-@st.cache(show_spinner=False)
+def convert_to_pdf(input_file, output_folder_path):
+    _, file_extension = os.path.splitext(input_file)
+    val = file_extension.lower()
+    if val == '.mhtml' or val == '.html' or val == '.htm' or val == '.igs' or val == '.xml':
+        url = f"file:///{os.path.abspath(input_file)}"
+
+        # Download the content of the file
+        response = urllib.request.urlopen(url)
+
+        # Create a BeautifulSoup object to parse the content
+        soup = BeautifulSoup(response.read(), "html.parser")
+
+        # Find the elements you want to scrape
+        data = soup.find_all()
+
+        # Convert the scraped data to a PDF
+        output_file_name = os.path.splitext(os.path.basename(input_file))[0] + ".pdf"
+        output_file_path = os.path.join(output_folder_path, output_file_name)
+        with open(output_file_path, "wb") as f:
+            html = "<html><head><meta charset='UTF-8'></head><body>"
+            for d in data:
+                html += str(d)
+            html += "</body></html>"
+            pisa.CreatePDF(html, dest=f)
+        print(f"File converted: {output_file_path}")
+        response.close()
+        os.remove(input_file)
+            
+   
+
 def scrape_files(input_folder_path, output_folder_path):
-    # Call the scrape_files function with the input and output folder paths
-    Topdftool.scrape_files(input_folder_path, output_folder_path)
+    # Get a list of all files in the input folder
+    input_files = [
+        os.path.join(input_folder_path, f) for f in os.listdir(input_folder_path)
+        if os.path.isfile(os.path.join(input_folder_path, f))
+    ]
+
+    # Create a process pool and convert each file to PDF using a separate process
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        results = []
+        for input_file in input_files:
+            try:
+                results.append(pool.apply_async(convert_to_pdf, args=(input_file, output_folder_path)))
+            except:
+                continue
+
+        for r in results:
+            r.wait()
+    
 
 @st.cache(show_spinner=False)
 def scrape_api():
@@ -15,9 +65,9 @@ def scrape_api():
 
     if st.button("Scrape"):
         scrape_files(input_folder_path, output_folder_path)
-        return JsonResponse({'status': 'success'})
+        return {'status': 'success'}
 
-    return JsonResponse({'status': 'waiting'})
+    return {'status': 'waiting'}
 
 
 def main():

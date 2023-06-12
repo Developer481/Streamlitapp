2712fd55d7c98f1f3bb49b4c05125a93ce319742
Developer481 <124763573+Developer481@users.noreply.github.com>
2023-06-12 15:44:17 +0530

    fdfdfdf

--- stapp1.py
+++ stapp1.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 import os
 import multiprocessing
 from bs4 import BeautifulSoup
@@ -5,7 +6,6 @@ import urllib.request
 from xhtml2pdf import pisa
 import shutil
 import streamlit as st
-import zipfile
 
 def convert_to_pdf(input_file, output_folder_path):
    
@@ -60,57 +60,20 @@ def scrape_files(input_folder_path, output_folder_path):
         for r in results:
             r.wait()
 
+
 def main():
     st.title("File Conversion App")
 
     # Get input and output folder paths from the user
-    input_zip = st.sidebar.file_uploader("Upload Input Folder Zip", type="zip")
+    input_folder_path = st.sidebar.text_input("Input Folder Path")
     output_folder_path = st.sidebar.text_input("Output Folder Path")
+    print(input_folder_path)
 
     # Convert files to PDF and display success message
-    if st.button("Convert Files") and input_zip is not None:
-        # Create a temporary folder to extract the uploaded zip file
-        temp_folder_path = "temp_folder"  # Relative path to the temporary folder
-        os.makedirs(temp_folder_path, exist_ok=True)
-
-        # Extract the zip file to the temporary folder
-        with zipfile.ZipFile(input_zip, 'r') as zip_ref:
-            zip_ref.extractall(temp_folder_path)
-
-        # Get the extracted input folder path
-        extracted_folder = zip_ref.namelist()[0]  # Assuming the zip contains only one top-level folder
-        extracted_folder_path = os.path.join(temp_folder_path, extracted_folder)
-
-        # Replace forward slashes with underscores in file/folder names
-        for root, dirs, files in os.walk(extracted_folder_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                new_file_path = file_path.replace('/', '_')
-                os.rename(file_path, new_file_path)
-            for dir in dirs:
-                dir_path = os.path.join(root, dir)
-                new_dir_path = dir_path.replace('/', '_')
-                os.rename(dir_path, new_dir_path)
-
-        # Print the extracted folder path for debugging
-        print("Extracted Folder Path:", extracted_folder_path)
-
-        # Convert files to PDF using the extracted folder
-        scrape_files(extracted_folder_path, output_folder_path)
-
-        # Remove the temporary folder
-        shutil.rmtree(temp_folder_path)
-
+    if st.button("Convert Files"):
+        scrape_files(input_folder_path, output_folder_path)
         st.success("Files converted successfully!")
 
-        # Show the converted PDF files to the user
-        if output_folder_path:
-            st.subheader("Converted PDF Files")
-            converted_files = os.listdir(output_folder_path)
-            for file in converted_files:
-                file_path = os.path.join(output_folder_path, file)
-                st.write(file_path)
-
-        st.success("Files converted successfully!")
 if __name__ == '__main__':
     main()
+
